dae23c3a9e78a1f4d53dac2ba2e8371540d140a5
f4gbv <f4gbv@free.fr>
2018-02-26 00:28:26 +0100

    màj 26 02 18 correction bug màj titre réseau

---
+++ spotnik/restart.el
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# DTMF 102#
+
+# Stop svxlink
+pkill -TERM svxlink
+sleep 2
+
+# Save network
+echo "el" > /etc/spotnik/network
+
+#/etc/spotnik/audio.sh
+
+
+sleep 1
+
+# Clear logs
+> /tmp/svxlink.log
+
+# Launch svxlink
+svxlink --daemon --logfile=/tmp/svxlink.log --pidfile=/var/run/svxlink.pid --runasuser=root --config=/etc/spotnik/svxlink.echo
+sleep 1
+
+# Enable EchoLink 
+echo "2#" > /tmp/svxlink_dtmf_ctrl_pty
+
+sleep 20m
+
+/etc/spotnik/restart.rrf
+
