4b76671651b1ebcdc80e18dce1d0516141cb2a27
f5nlg <f5nlg@users.noreply.github.com>
2019-07-20 13:56:59 +0200

    Delete cron_conserve.sh

--- cron_conserve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-/etc/svxlink/conserve_log.sh /tmp/svxlink.log
-/etc/svxlink/conserve_log.sh /tmp/technique.log
-/etc/svxlink/conserve_log.sh /tmp/svxreflector.log
-/etc/svxlink/conserve_log.sh /tmp/svxreflector1.log 
-/etc/svxlink/conserve_log.sh /tmp/svxreflector2.log 
-
